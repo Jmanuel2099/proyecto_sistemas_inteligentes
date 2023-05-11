7a766e946d03e3f97aacee96666f6a94cd9b0daf
Jose Manuel Marin <jmanuel2099@gmail.com>
2023-05-11 09:34:18 -0500

    mongo conection string updated

--- proyecto_sistemas_inteligentes/mongodb.py
+++ proyecto_sistemas_inteligentes/mongodb.py
@@ -1,9 +1,10 @@
 from pymongo import MongoClient
+from proyecto_sistemas_inteligentes.environment import PASSWORD, USER
 
-
-COLLECTION_TEST_NAME = 'test'
+PROJECT_NAME='proyectofinal'
 DATABASE_NAME = 'micro-servicios'
+COLLECTION_TEST_NAME = 'test'
 
-client = MongoClient('mongodb+srv://Jose:Jose0920@proyectofinal.afvv5vl.mongodb.net/')
+client = MongoClient(f'mongodb+srv://{USER}:{PASSWORD}@{PROJECT_NAME}.afvv5vl.mongodb.net/')
 db = client.get_database(DATABASE_NAME)
 db_collection_test = db.get_collection(COLLECTION_TEST_NAME)
