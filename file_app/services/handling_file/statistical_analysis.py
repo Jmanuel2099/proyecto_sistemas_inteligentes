afc2bcc3a9f744c7e0e55fcd68a24256b7fb31db
Jose Manuel Marin <jmanuel2099@gmail.com>
2023-05-18 17:55:04 -0500

    The commented code is cleaned

--- file_app/services/handling_file/statistical_analysis.py
+++ file_app/services/handling_file/statistical_analysis.py
@@ -15,42 +15,6 @@ class StatisticalAnalysis:
     def __init__(self, file) -> None:
         self.file = file
 
-    # def discard(self):
-    #     try:
-    #         data_frame = self.file.get_data_frame()
-    #         data_without_NaN = data_frame.dropna()
-
-    #         self.file.set_df_not_missing_data(data_without_NaN)
-    #         self.file.save_df_not_missing_data_in_local(self.DATAFRAME_FOLDER_WITH_DESCARD)
-    #     except Exception as error:
-    #         raise error
-
-    # def imputation(self):
-    #     try:
-    #         data_frame = self.file.get_data_frame()
-    #         num_columns = data_frame.select_dtypes(np.number).columns
-    #         obj_columns = data_frame.select_dtypes(object).columns
-
-    #         data_frame = self._average_imputation(data_frame, num_columns)
-    #         data_frame = self._mode_imputation(data_frame, obj_columns)
-
-    #         self.file.set_df_not_missing_data(data_frame)
-    #         self.file.save_df_not_missing_data_in_local(self.DATAFRAME_FOLDER_WITH_AVERAGE_IMPUTATION)
-    #     except Exception as error:
-    #         raise error
-
-    # def _average_imputation(self, data_frame, numeric_columns):
-    #     for column in numeric_columns:
-    #         media = data_frame[column].mean()
-    #         data_frame[column].fillna(media, inplace=True)
-    #     return data_frame
-
-    # def _mode_imputation(self, data_frame, not_numeric_columns):
-    #     for column in not_numeric_columns:
-    #         mode = data_frame[column].mode()[0]
-    #         data_frame[column].fillna(mode, inplace=True)
-    #     return data_frame
-
     def histograms(self):
         try:
             data_frame = self.file.get_df_not_missing_data()
